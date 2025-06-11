b367c0645a07a50135f313f45ea7a636a5efa3a4
CKATEPTb <ckateptb@vivaldi.net>
2025-06-11 08:00:14 +0300

    Fix application.properties

--- src/main/java/com/example/name/Application.java
+++ src/main/java/com/example/name/Application.java
@@ -8,7 +8,13 @@ import net.lenni0451.reflect.Agents;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.Properties;
 import java.util.Set;
 
 @Slf4j
@@ -24,7 +30,20 @@ public class Application {
         new SpringApplicationBuilder(clazz)
                 .headless(true)
                 .bannerMode(Banner.Mode.OFF)
-                .initializers(ctx -> ctx.setClassLoader(classLoader))
+                .initializers(ctx -> {
+                    ctx.setClassLoader(classLoader);
+                    ConfigurableEnvironment environment = ctx.getEnvironment();
+                    try (InputStream input = classLoader.getResourceAsStream("application.properties")) {
+                        if (input != null) {
+                            Properties props = new Properties();
+                            props.load(input);
+                            PropertiesPropertySource source = new PropertiesPropertySource("properties", props);
+                            environment.getPropertySources().addLast(source);
+                        }
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
                 .run(args);
         Set<String> transformers = transformer.getRegisteredTransformer();
         log.info("Registered mixin's ({}): {}", transformers.size(), transformers);
